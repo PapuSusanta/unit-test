55d8ec110473320094dbc310911eb0dfe35cbc5f
Susanta <86820961+PapuSusanta@users.noreply.github.com>
2026-08-18 21:13:57 +0530

    test calculator

--- src/Calculator/Calculation.cs
+++ src/Calculator/Calculation.cs
@@ -1,6 +1,12 @@
 ﻿namespace Calculator;
 
-public class Class1
+public class Calculation
 {
-
+    public int Add(int a, int b) => a + b;
+    
+    public int Subtract(int a, int b) => a - b;
+    
+    public int Multiply(int a, int b) => a * b;
+    
+    public int Divide(int a, int b) => a / b;
 }
--- test/Test.Calculator/TestCalculator.cs
+++ test/Test.Calculator/TestCalculator.cs
@@ -1,14 +1,37 @@
 using Calculator;
-
 namespace Test.Calculator;
 
-public class TestMultiplication
+public class TestCalculator
 {
     [Fact]
-    public void Multiply_TwoNumber_ShouldReturnResult()
+    public void Add_TowNumbers_ShoulsReturnsCorrectResult()
     {
         var calculator = new Calculation();
-        var result = calculator.Multiply(2, 2);
+        var result = calculator.Add(1, 2);
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void Subtract_TowNumbers_ShoulsReturnsCorrectResult()
+    {
+        var calculator = new Calculation();
+        var result = calculator.Subtract(1, 2);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Multiply_TowNumbers_ShoulsReturnsCorrectResult()
+    {
+        var calculator = new Calculation();
+        var result = calculator.Multiply(1, 2);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Divide_TowNumbers_ShoulsReturnsCorrectResult()
+    {
+        var calculator = new Calculation();
+        var result = calculator.Divide(4, 2);
         Assert.Equal(2, result);
     }
 }
